17f86ec8fc831773df37599e3a755f6ad055a82a
gabriellanna2006 <gabriel132lanna@gmail.com>
2026-05-12 09:21:37 -0300

    Adicionando tabelas pedidos e item_pedidos

---
+++ app/Models/Pedido.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Pedido extends Model
+{
+    protected $table = 'pedidos';
+
+    protected $fillable = [
+        'user_id',
+        'data_pedido',
+        'status'
+    ];
+
+    public function usuario()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function itens()
+    {
+        return $this->hasMany(ItemPedido::class, 'pedido_id');
+    }
+}
